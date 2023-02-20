afe4b9178ce8d863e675a2755de7f051e4a79fca
jiahoe <mrjiahoe@hotmail.com>
2023-02-20 21:41:46 +0800

    done but error fetching from cloudinary

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { generatePath, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import { preview } from '../assets';
 import { getRandomPrompt } from '../utils';
@@ -7,13 +7,23 @@ import { FormField, Loader } from '../components';
 
 const CreatePost = () => {
 	const navigate = useNavigate();
+
 	const [form, setForm] = useState({
 		name: '',
 		prompt: '',
 		photo: '',
 	});
+
 	const [generatingImg, setGeneratingImg] = useState(false);
 	const [loading, setLoading] = useState(false);
+
+	const handleChange = (e) =>
+		setForm({ ...form, [e.target.name]: e.target.value });
+
+	const handleSurpriseMe = () => {
+		const randomPrompt = getRandomPrompt(form.prompt);
+		setForm({ ...form, prompt: randomPrompt });
+	};
 
 	const generateImage = async () => {
 		if (form.prompt) {
@@ -24,58 +34,76 @@ const CreatePost = () => {
 					headers: {
 						'Content-Type': 'application/json',
 					},
-					body: JSON.stringify({ prompt: form.prompt }),
+					body: JSON.stringify({
+						prompt: form.prompt,
+					}),
 				});
 
 				const data = await response.json();
-
 				setForm({ ...form, photo: `data:image/jpeg;base64,${data.photo}` });
-			} catch (error) {
-				alert(error);
+			} catch (err) {
+				alert(err);
 			} finally {
 				setGeneratingImg(false);
 			}
 		} else {
-			alert('Please enter a prompt');
+			alert('Please provide proper prompt');
 		}
 	};
 
-	const handleSubmit = () => {};
+	const handleSubmit = async (e) => {
+		e.preventDefault();
 
-	const handleChange = (e) => {
-		setForm({ ...form, [e.target.name]: e.target.value });
-	};
+		if (form.prompt && form.photo) {
+			setLoading(true);
+			try {
+				const response = await fetch('http://localhost:8080/api/v1/post', {
+					method: 'POST',
+					headers: {
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({ ...form }),
+				});
 
-	const handleSurpriseMe = () => {
-		const randomPrompt = getRandomPrompt(form.prompt);
-		setForm({ ...form, prompt: randomPrompt });
+				await response.json();
+				alert('Success');
+				navigate('/');
+			} catch (err) {
+				alert(err);
+			} finally {
+				setLoading(false);
+			}
+		} else {
+			alert('Please generate an image with proper details');
+		}
 	};
 
 	return (
 		<section className="max-w-7xl mx-auto">
 			<div>
 				<h1 className="font-extrabold text-[#222328] text-[32px]">Create</h1>
-				<p className="mt-2 text-[#666e75] text-[16px] max-w-[500px]">
-					Create imaginative and visually stunning images through DALL-E AI and
-					share them with the community
+				<p className="mt-2 text-[#666e75] text-[14px] max-w-[500px]">
+					Generate an imaginative image through DALL-E AI and share it with the
+					community
 				</p>
 			</div>
 
 			<form className="mt-16 max-w-3xl" onSubmit={handleSubmit}>
 				<div className="flex flex-col gap-5">
 					<FormField
-						labelName="Your name"
+						labelName="Your Name"
 						type="text"
 						name="name"
-						placeholder="John Doe"
+						placeholder="Ex., john doe"
 						value={form.name}
 						handleChange={handleChange}
 					/>
+
 					<FormField
 						labelName="Prompt"
 						type="text"
-						name="Prompt"
-						placeholder="A plush toy robot sitting against a yellow wall"
+						name="prompt"
+						placeholder="An Impressionist oil painting of sunflowers in a purple vase…"
 						value={form.prompt}
 						handleChange={handleChange}
 						isSurpriseMe
@@ -109,7 +137,7 @@ const CreatePost = () => {
 					<button
 						type="button"
 						onClick={generateImage}
-						className="text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
+						className=" text-white bg-green-700 font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
 					>
 						{generatingImg ? 'Generating...' : 'Generate'}
 					</button>
@@ -117,14 +145,14 @@ const CreatePost = () => {
 
 				<div className="mt-10">
 					<p className="mt-2 text-[#666e75] text-[14px]">
-						Once you have created the image you want, you can share it with
-						others in the community
+						** Once you have created the image you want, you can share it with
+						others in the community **
 					</p>
 					<button
 						type="submit"
 						className="mt-3 text-white bg-[#6469ff] font-medium rounded-md text-sm w-full sm:w-auto px-5 py-2.5 text-center"
 					>
-						{loading ? 'Sharing...' : 'Share with the community'}
+						{loading ? 'Sharing...' : 'Share with the Community'}
 					</button>
 				</div>
 			</form>
